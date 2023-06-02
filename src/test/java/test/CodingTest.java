34c6288d3d03a449f84f3f76d924121af072ed76
dell <dell@BALAJI>
2023-06-02 16:02:57 +0530

    code

--- src/test/java/test/CodingTest.java
+++ src/test/java/test/CodingTest.java
@@ -1,5 +1,31 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CodingTest {
+
+	public static void main(String[] args)
+	{
+		ArrayList<String> al=new ArrayList<String>(3);
+		al.add("s1");
+		al.add("s2");
+		al.add("s3");
+		int j=1;
+		while(j<10){
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter your Option");
+			String op = sc.nextLine();
+			al.add(op);
+			al.remove(0);	  
+			for(String i:al)
+			{
+				System.out.println(i);
+			}
+			j--;
+		}
+	}
+
+
 
 }
